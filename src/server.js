296d4744a678451eb182c1fe77b2609b2eac0e8e
sweeneyb <tab126@gmail.com>
2022-04-10 11:50:42 -0400

    run the test on a schedule so metrics are immediately available to requests

--- src/server.js
+++ src/server.js
@@ -5,25 +5,39 @@ const promFormatter = require('./prom-formatter');
 
 const app = new Koa();
 
+var minutes = 1, the_interval = minutes * 60 * 1000;
+
+let testResults;
+let upperV = {}
+
+function test() {
+  let test = new SpeedTest();
+  test.run()
+    .then(v => {
+      upperV = v
+      testResults = promFormatter.format(v);
+      console.log('speedtest: ', {download: v.speeds.download, upload: v.speeds.upload, ping: v.server.ping});
+    })
+    .catch(e => {
+      console.log('e', e);
+    });
+}
+
+setInterval(test, the_interval)
+test()
+
 const routes = {
   metrics: async ctx => {
     console.log('/metrics');
-    let testResults;
-    let test = new SpeedTest();
-    await test.run()
-      .then(v => {
-        testResults = promFormatter.format(v);
-        console.log('speedtest: ', {download: v.speeds.download, upload: v.speeds.upload, ping: v.server.ping});
-      })
-      .catch(e => {
-        console.log('e', e);
-      });
+
+
     ctx.type = 'text/plain; version=0.0.4';
-    ctx.body = testResults;
+    ctx.body = promFormatter.format(upperV);;
 
   }
 };
 
+app.use(_.get('/metrics', routes.metrics));
 app.use(_.get('/metrics/', routes.metrics));
 
 app.listen(9696);
